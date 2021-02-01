477bbc88e6bff583ee6a8d170cdb0cde9fcbcf45
Leonid Posadskov <lposadskov@gmail.com>
2021-02-01 17:17:01 +0900

    Part 3-3

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = (principal * years * rate)/ 100;
     var endyear = new Date().getFullYear() + years;
-    document.getElementById("rateres").innerHTML = "If you deposit "+principal+",\nat an interest rate of "+rate+"%.\nYou will receive an amount of "+interest+",/nin a year " + endyear;
+    document.getElementById("result").innerHTML = "If you deposit "+principal+",\nat an interest rate of "+rate+"%.\nYou will receive an amount of "+interest+",/nin a year " + endyear;
 }
-
-document.getElementById("rate").onchange = function() {rateUpdate()};
 
 function rateUpdate() 
 {
