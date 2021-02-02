30a09a1464892962278e8bbd2cfd0684d15c216a
Leonid Posadskov <lposadskov@gmail.com>
2021-02-02 18:03:42 +0900

    Part 4 fix 1

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = (principal * years * rate)/ 100;
     var endyear = new Date().getFullYear() + parseInt(years, 10);
-    document.getElementById("result").innerHTML = "If you deposit "+principal+",<br />at an interest rate of "+rate+"%.<br />You will receive an amount of "+interest+",<br />in a year " + endyear;
+    if (parseInt(principal,10) >= 0)
+    {
+        alert("Enter a positive number");
+    }
+    else
+    {
+        var interest = (principal * years * rate)/ 100;
+        document.getElementById("result").innerHTML = "If you deposit <mark>"+principal+"</mark>,<br />at an interest rate of <mark>"+rate+"%</mark>.<br />You will receive an amount of <mark>"+interest+"</mark>,<br />in a year <mark>" + endyear + "</mark>";
+    }
 }
 function rateUpdate()
 {
